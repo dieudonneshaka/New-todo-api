80372cbda98e60d7692bc69365f6998e87adfd7f
dieudonneshaka <158247669+dieudonneshaka@users.noreply.github.com>
2025-07-24 01:00:50 +0300

    last commit

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58f6613777c771079976e20c93c879b6dff2978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c1d42eae621d395295e4d52c69d49c73022cfb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
